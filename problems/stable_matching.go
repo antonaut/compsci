174f0e9e96446498afad500d3342572d345689ff
Anton Erholt <anton.erholt@gmail.com>
2016-09-03 01:00:43 +0200

    Reindexed, fixed params + comments

--- problems/stable_matching.go
+++ problems/stable_matching.go
@@ -5,9 +5,20 @@ type node struct {
 	next *node
 }
 
-// This is an Theta(n^2) implementation of a solution to the stable matching
-// problem
-func stable_match(men_pref [][]int, women_pref [][]int, n int) []int {
+// This is a `\Theta(n^2)` implementation of a solution to the stable matching
+// (marriage) problem. More info at:
+// https://en.wikipedia.org/wiki/Stable_marriage_problem
+
+// Women and men are represented as int\`s from `[1..n]` both inclusive. Every
+// woman needs to rank every man and vice versa in their preference lists.
+
+// This function takes preference lists from both men and women and returns a
+// slice `husband[w]` saying that woman `w` should be married to husband
+// `husband[w]`.
+func stable_match(men_pref [][]int, women_pref [][]int) []int {
+
+	n := len(men_pref)
+
 	next := make([]int, n+1)
 	current := make([]int, n+1)
 	rank := make([][]int, n+1)
@@ -22,13 +33,13 @@ func stable_match(men_pref [][]int, women_pref [][]int, n int) []int {
 		free_men = man_node
 
 		for j := 0; j < n; j++ {
-			rank[i][women_pref[i][j]] = j
+			rank[i][women_pref[i-1][j]] = j
 		}
 	}
 
 	for free_men != nil {
 		man := free_men.man
-		target_bride := men_pref[man][next[man]]
+		target_bride := men_pref[man-1][next[man]]
 		next[man]++
 		if current[target_bride] == 0 {
 			current[target_bride] = man
--- problems/stable_matching_test.go
+++ problems/stable_matching_test.go
@@ -7,20 +7,18 @@ import (
 func TestStableMatch(t *testing.T) {
 	t.Log("Det här är svinbra.")
 	men_prefs := [][]int{
-		[]int{},
 		[]int{1, 2, 3},
 		[]int{2, 1, 3},
 		[]int{1, 2, 3},
 	}
 	women_prefs := [][]int{
-		[]int{},
 		[]int{1, 2, 3},
 		[]int{2, 3, 1},
 		[]int{3, 1, 2},
 	}
-	n := 3
+	n := len(men_prefs)
 
-	matching := stable_match(men_prefs, women_prefs, n)
+	matching := stable_match(men_prefs, women_prefs)
 
 	for i := 1; i <= n; i++ {
 		t.Logf("m: %d, w: %d", matching[i], i)
